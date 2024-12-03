7fdf5ab1ca35569c0730eeccac370552577975fc
ryan.bardahi <ryan.bardahi@gmail.com>
2024-12-03 21:10:04 +0800

    Hello, world!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
+  const isLoggedIn = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header>
+          <h1 className="page-title">the meditation notes</h1>
+        </header>
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li><Link to="/notes">notes</Link></li>
+            <li><Link to="/resources">resources</Link></li>
+            <li><Link to="/talk-to-the-universe">talk to the universe</Link></li>
+            {!isLoggedIn && (
+              <>
+                <li><Link to="/login">login</Link></li>
+                <li><Link to="/register">register</Link></li>
+              </>
+            )}
+            {isLoggedIn && <li><Link to="/logout">logout</Link></li>}
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      {/* Additional home page content goes here */}
+    </div>
+  );
+};
+
+export default Home;
